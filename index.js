a7686ac2cee3e92a4ce0fdc4501e1607c9fa9741
Dhee0raj23 <dheeroy00@gmail.com>
2023-03-02 10:47:35 +0530

    change backgrond color and font changed to bold

---
+++ index.js
@@ -0,0 +1,4 @@
+let items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+let allItems = document.getElementById("items");
+allItems.style.fontWeight="bold";
