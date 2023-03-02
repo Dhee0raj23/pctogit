88d8c0fe5a5236419ae3146f24af5b9ef3218b69
Dhee0raj23 <dheeroy00@gmail.com>
2023-03-02 16:42:22 +0530

    queryslector

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 
-// let items = document.getElementsByClassName('list-group-item');
-// items[2].style.backgroundColor = 'green';
-// let allItems = document.getElementById("items");
-// allItems.style.fontWeight="bold";
+// // let items = document.getElementsByClassName('list-group-item');
+// // items[2].style.backgroundColor = 'green';
+// // let allItems = document.getElementById("items");
+// // allItems.style.fontWeight="bold";
 
 
-//get element by tag name;
-// let li = document.getElementsByTagName('li');
-// li[4].textContent="hello";
-// li[4].style.backgroundColor="red"
-// li[4].style.color="blue";
+// //get element by tag name;
+// // let li = document.getElementsByTagName('li');
+// // li[4].textContent="hello";
+// // li[4].style.backgroundColor="red"
+// // li[4].style.color="blue";
 
-//get element by class name;
-let newList = document.getElementsByClassName("new-list-item")
-newList[0].textContent="hello";
-newList[0].style.backgroundColor="red";
-newList[0].style.color="blue";
+// //get element by class name;
+// let newList = document.getElementsByClassName("new-list-item")
+// newList[0].textContent="hello";
+// newList[0].style.backgroundColor="red";
+// newList[0].style.color="blue";
 
+  //queryslector-->only use for one item;
+  let SecItem = document.querySelector(".list-group-item:nth-child(2)");
+  SecItem.style.backgroundColor="green";
+
+ let thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+ thirdItem.style.display="none";
